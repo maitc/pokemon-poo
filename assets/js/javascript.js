f44daaae107e3c021b64ebea1b7714ec47e956eb
maitc <maibeet.torres@gmail.com>
2017-05-04 21:27:19 -0400

    se agrega función constructora

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,24 @@
 function boton(){//función principal que al accionar click ase activa.
 
+	function Pokemon(nombre, color, poderDeAtaque){//constructor por donde pasaran todos los pokemones que yo quiera.
+			this.nombre = nombre;
+			this.color = color;
+			this.nivelDeAmistad = 0;
+			this.vida = 100;
+			this.poderDeAtaque = poderDeAtaque;
+			
+			this.mostrarPokemon = function(){
+				return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
+			}
 
+			this.aumentarAmistad = function(valor){
+				this.nivelDeAmistad = this.nivelDeAmistad + valor;
+			}
+
+			this.atacar = function(pokemon){
+				return pokemon.vida = pokemon.vida - this.poderDeAtaque;
+			}
+		}
 
 }
 
