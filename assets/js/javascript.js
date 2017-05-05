b1da54f213025303c339d8aef44d580114cedfe6
maitc <maibeet.torres@gmail.com>
2017-05-04 21:29:44 -0400

    se agrega select a constructor con los pokemones que quiero y se imprime resultado a traves del inner al html

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -24,6 +24,12 @@ function boton(){//función principal que al accionar click ase activa.
 var select1 = document.getElementById("pikac").value;//se trae select.
 var select2 = document.getElementById("charma").value;
 
+var pik = new Pokemon(select1, "amarillo", 50);//se une al constructor.
+var char = new Pokemon(select2, "rojo", 20);
+
+var respuesta = document.getElementById("res");//donde se mostrará el resultado de la batalla.
+respuesta.innerHTML = "Pokémon " + pik.nombre + " atacó a Pokémon " + char.nombre + " y pokémon " + char.nombre + " tiene " + char.vida + " de vida restante.";
+
 }
 
 
