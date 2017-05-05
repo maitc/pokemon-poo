9fd2d7e74b53d3ec5f14fb664633d0d1d4ea1e07
maitc <maibeet.torres@gmail.com>
2017-05-04 21:26:28 -0400

    se realiza función de botón mas index con select ya ok

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,25 +1,9 @@
-function Pokemon(nombre, color, poderDeAtaque){
-	this.nombre = nombre;
-	this.color = color;
-	this.nivelDeAmistad = 0;
-	this.vida = 100;
-	this.poderDeAtaque = poderDeAtaque;
-	
-	this.mostrarPokemon = function(){
-		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
-	}
+function boton(){//función principal que al accionar click ase activa.
 
-	this.aumentarAmistad = function(valor){
-		this.nivelDeAmistad = this.nivelDeAmistad + valor;
-	}
 
-	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida - this.poderDeAtaque;
-	}
+
 }
 
-var pikachu = new Pokemon("Pikachu", "amarillo", 50);
-var charmander = new Pokemon("Charmander", "rojo", 20);
 
-pikachu.atacar(charmander);
-console.log(charmander.vida);
+
+
